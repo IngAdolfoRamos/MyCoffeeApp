62c88425e91c60fc0e718fd3b46f017ed2946e19
Adolfo Ramos <ingadolfo@icloud.com>
2022-01-22 17:53:34 -0600

    Add project files.

---
+++ MyCoffeeApp/MyCoffeeApp/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MyCoffeeApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyCoffeeApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
---
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyCoffeeApp.ViewModels
+{
+    internal class CoffeeEquipmentViewModel : ViewModelBase
+    {
+        public CoffeeEquipmentViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+
+        }
+
+        public ICommand IncreaseCount { get; }
+
+        int count = 0;
+        string countDisplay = "Welcome to Coffee Equipment";
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set => SetProperty(ref countDisplay, value);
+        }
+
+        void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"You clicled {count} times";
+        }
+    }
+}

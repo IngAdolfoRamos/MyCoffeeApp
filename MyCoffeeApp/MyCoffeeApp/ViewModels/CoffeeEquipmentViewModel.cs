52004aff1badc3756cd19e23bf8be7a3e8662b47
Adolfo Ramos <ingadolfo@icloud.com>
2022-01-28 19:22:44 -0600

    Binding propertys in a ListView, when using BindingCintext, do not use x:DataType, propertys unrecognizables

--- MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using MyCoffeeApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -9,27 +13,34 @@ namespace MyCoffeeApp.ViewModels
 {
     internal class CoffeeEquipmentViewModel : ViewModelBase
     {
+
+        public ObservableRangeCollection<Coffee> Coffee { get; set; }
+        public ObservableRangeCollection<Grouping<string, Coffee>> CoffeeGroups { get; }
+
+        public AsyncCommand RefreshCommand { get; }
+
         public CoffeeEquipmentViewModel()
         {
-            //command instead of click button handler event
-            IncreaseCount = new Command(OnIncrease);
+            Title = "Coffee Equipment";
+            Coffee = new ObservableRangeCollection<Coffee>();
+            CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+            var image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBoXyezjniIUJyXeKOPTyj466Nb6eCCrXUNg&usqp=CAU";
 
+            Coffee.Add(new Coffee { Roaster = "UNO", Name = "NOMBRE", Image = image });
+            Coffee.Add(new Coffee { Roaster = "DOS", Name = "NOMBRE", Image = image });
+            Coffee.Add(new Coffee { Roaster = "TRES", Name = "NOMBRE", Image = image });
+
+            CoffeeGroups.Add(new Grouping<string, Coffee>("TRES", new[] { Coffee[2] }));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("DOS", Coffee.Take(2)));
+
+            RefreshCommand = new AsyncCommand(Refresh);
         }
 
-        public ICommand IncreaseCount { get; }
-
-        int count = 0;
-        string countDisplay = "Welcome to Coffee Equipment";
-        public string CountDisplay
+        async Task Refresh()
         {
-            get => countDisplay;
-            set => SetProperty(ref countDisplay, value);
-        }
-
-        void OnIncrease()
-        {
-            count++;
-            CountDisplay = $"You clicled {count} times";
+            IsBusy = true;
+            await Task.Delay(2000);
+            IsBusy = false;
         }
     }
 }
